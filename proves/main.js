6863125067a26769b3ce5bf783e6392cb6f0c6af
Juan Ojeda Duran <joan.ojeda@gmail.com>
2013-05-08 23:23:55 +0200

    init

---
+++ proves/main.js
@@ -0,0 +1,15 @@
+function getSessions(){
+//recuperem els blogs/sessions que han de sortir a la llista principal
+
+
+}
+
+function getSessionIcon(){
+//recuperem el sessionIcon o Avatar d'un blog
+
+}
+
+function getPhotos(){
+//recuperem les photos d'un blog
+
+}
