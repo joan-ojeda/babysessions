5a1eb6be1b05b8e15ea2bf3578bf2753937ac26e
Juan Ojeda Duran <joan.ojeda@gmail.com>
2013-05-20 23:12:20 +0200

    init 2

--- bs.js
+++ bs.js
@@ -1,3 +1,39 @@
+
+
+/* jojeda 2013
+ * Canviar totes les ocurrències d'una cadena per una altre
+ */
+ 
+function replaceAll(text,busca,reemplaza){
+  while (text.toString().indexOf(busca) != -1)
+      text = text.toString().replace(busca,reemplaza);
+	return text;
+}
+
+/* jojeda 2013
+ * Neteja les etiquetes extres que afegeix el servidor
+ */
+function cleanExtraTags(text){
+	text=text.replace("<ul>","");
+	text=text.replace("</ul>","");
+	text=text.replace("<p>","");
+	text=text.replace("</p>","");
+	text=replaceAll(text,"http://","");
+	return text;
+}
+
+/* jojeda 2013
+ * Donar contingut a un iframe, és a dir carregar la url que ha de mostrar
+ */
+function loadIframe(iframeName, url) {
+    if ( window.frames[iframeName] ) {
+        //window.frames[iframeName].location = "http://www.uoc.edu";   
+		window.frames[iframeName].location = url;   
+        return false;
+    }
+    return true;
+}
+
 /* Copyright (c) 2006 Mathias Bank (http://www.mathias-bank.de)
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
